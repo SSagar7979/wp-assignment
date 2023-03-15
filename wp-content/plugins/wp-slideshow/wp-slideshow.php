7a00a74859de01ba6676f71c8fe65b02e10a1e68
SagarSavani <savanisagar99@gmail.com>
2023-03-15 14:22:59 +0530

    Admin menu page

--- wp-content/plugins/wp-slideshow/wp-slideshow.php
+++ wp-content/plugins/wp-slideshow/wp-slideshow.php
@@ -7,8 +7,39 @@
  * Author URI:      https://github.com/SSagar7979
  * Text Domain:     wp-slideshow
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0
  *
  * @package         Wp_Slideshow
  */
 
+ // Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+new Wordpress_SlideShow();
+
+class Wordpress_SlideShow{     
+     public function __construct(){
+
+          // Add admin menu item and settings page
+          add_action( 'admin_menu', array($this,'wp_slideshow_plugin_admin_menu') );          
+     }
+
+     protected function wp_slideshow_plugin_admin_menu() {
+          add_menu_page(
+               'WP Slideshow',
+               'Slideshow',
+               'manage_options',
+               'wp-slideshow',
+               array($this,'my_slideshow_plugin_options_page')
+          );
+     }
+
+     protected function my_slideshow_plugin_options_page() {
+          ?>
+          <div class="wrap">
+              <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+              
+          </div>
+          <?php
+     }
+}
