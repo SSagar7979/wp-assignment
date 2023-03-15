491a7502e40eca3c5ee1877fecc8817ecc5e8cf4
SagarSavani <savanisagar99@gmail.com>
2023-03-15 17:51:52 +0530

    PHPunit test changes

--- wp-content/plugins/wp-slideshow/_assets/js/scripts.js
+++ wp-content/plugins/wp-slideshow/_assets/js/scripts.js
@@ -1,27 +1,31 @@
+/**
+ * WordPress Slideshow script file
+ */
 
-
-function add_my_plugin_media() {
-     var mediaUploader = wp.media({
-          multiple: true // allow multiple selection
-     });
-     mediaUploader.on('select', function() {
-          var selection = mediaUploader.state().get('selection');
-          var mediaArray = [];
-          selection.map(function(attachment) {
-               attachment = attachment.toJSON();
-               mediaArray.push(attachment.id);
-          });
-          
-          jQuery.post(
-               ajax_obj.url,
-               {
-                    action: 'wp_slideshow_images_ajax',
-                    wp_slideshow_images: mediaArray
-               },
-               function( response ) {
-                    console.log( 'Media array saved!' );
-               }
-          );
-     });
-     mediaUploader.open();
-}
+jQuery( function ( $ ) {
+	$( document ).on( 'click', '#wp-slideshow-plugin-media-button', function () {
+		var mediaUploader = wp.media({
+			multiple: true,
+		});
+		mediaUploader.on('select', function() {
+			var selection  = mediaUploader.state().get( 'selection' );
+			var mediaArray = [];
+			selection.map(function( attachment ) {
+				attachment = attachment.toJSON();
+				mediaArray.push( attachment.id );
+			});
+			jQuery.post(
+				ajax_obj.url,
+				{
+					action: 'wp_slideshow_images_ajax',
+					nonce: ajax_obj.nonce,
+					wp_slideshow_images: mediaArray
+				},
+				function( response ) {
+					console.log( 'Media array saved!' );
+				}
+			);
+		});
+		mediaUploader.open();
+	});
+});
