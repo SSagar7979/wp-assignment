0da7b3e10d644ec91ad031da5e767835b941a719
SagarSavani <savanisagar99@gmail.com>
2023-03-15 13:53:51 +0530

    WP Slideshow plugin setup by wpcli

---
+++ wp-content/plugins/wp-slideshow/wp-slideshow/wp-slideshow.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Wordpress SlideShow Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     This is a assignment slideshow plugin.
+ * Author:          Sagar Savani
+ * Author URI:      https://github.com/SSagar7979
+ * Text Domain:     wp-slideshow
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Slideshow
+ */
+
