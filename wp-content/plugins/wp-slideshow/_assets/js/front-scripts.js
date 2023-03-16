fad9308d12d14adc67f058c88f98a042c2fa1d12
SagarSavani <savanisagar99@gmail.com>
2023-03-16 14:20:19 +0530

    Add slick files and shortcode functionality

--- wp-content/plugins/wp-slideshow/_assets/js/front-scripts.js
+++ wp-content/plugins/wp-slideshow/_assets/js/front-scripts.js
@@ -3,5 +3,11 @@
  */
 
 jQuery( function ( $ )  {
-	
+	$('.slideshow-images').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
 } );
--- wp-content/plugins/wp-slideshow/inc/class-wordpress-slideshow-front-end.php
+++ wp-content/plugins/wp-slideshow/inc/class-wordpress-slideshow-front-end.php
@@ -29,19 +29,37 @@ class WordPress_SlideShow_Front_End {
 	 * SlideShow enqueue front scripts callback function
 	 */
 	public function wp_slideshow_plugin_enqueue_front_scripts() {
+		// Slick CSS & JS files
+		wp_enqueue_style('slick-css', SIDESHOW__PLUGIN_URL . '_assets/css/slick.css');
+		wp_enqueue_style('slick-theme-css', SIDESHOW__PLUGIN_URL . '_assets/css/slick-theme.css');
+		wp_enqueue_script('slick-min-js', SIDESHOW__PLUGIN_URL . '_assets/js/slick.min.js', array('jquery'), SIDESHOW__VERSION, true );	
 
 		// enqueue custom js.
-		wp_enqueue_script( 'wp-slideshow-js', SIDESHOW__PLUGIN_URL . '_assets/js/front-scripts.js', array(), SIDESHOW__VERSION, true );
+		wp_enqueue_script( 'wp-slideshow-js', SIDESHOW__PLUGIN_URL . '_assets/js/front-scripts.js', array('jquery'), SIDESHOW__VERSION, true );
 	}
 
 	/**
 	 * SlideShow [myslideshow] callback function
 	 */
 	public function myslideshow_shortcode_callback(  ) {
-		
+		// Get current images.
+		$images = get_option( 'wp_slideshow_images', array() );
+
 		ob_start();
 		?>
-
+		<div class="slider slideshow-images">
+			<?php
+			foreach ( $images as $key => $image_id ) :
+				?>
+				<div class="slide-image slide-<?php echo esc_attr( $image_id ); ?>">
+					<?php
+						echo wp_get_attachment_image( $image_id, 'full', '', array( 'class' => 'img-responsive' ) );
+					?>
+				</div>
+				<?php
+			endforeach;
+			?>
+		</div>
 		<?php		
 		return ob_get_clean();
 	
